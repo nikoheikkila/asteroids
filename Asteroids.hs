40f73b4f2cb899ada62d7ea826a1c5f37da4cbb4
kamapas <katri.m.passi@student.jyu.fi>
2015-09-26 17:11:38 +0300

    Update Asteroids.hs

--- Asteroids.hs
+++ Asteroids.hs
@@ -109,10 +109,10 @@ drawWorld :: AsteroidWorld -> Picture
 
 drawWorld GameOver 
    = pictures [scale 0.3 0.3 . translate (-400) 0 
-     	       . color red . text $ "Game Over!",
-	       scale 0.1 0.1 . translate (-1150) (-550)
-	       . color white . text $ 
-	       "Click right mousebutton to restart"]
+               . color red . text $ "Game Over!",
+           scale 0.1 0.1 . translate (-1150) (-550)
+           . color white . text $ 
+           "Click right mousebutton to restart"]
 
 drawWorld (Play rocks (Ship (x,y) (vx,vy)) (UFO (ux,uy) (uvx, uvy)) bullets)
   = pictures [ship, asteroids, ufo, shots]
@@ -128,7 +128,7 @@ handleEvents :: Event -> AsteroidWorld -> AsteroidWorld
 
 -- new eventhandler for restarting --
 handleEvents (EventKey (MouseButton RightButton) Down _ _) GameOver
-	      = initialWorld
+          = initialWorld
 
 
 handleEvents (EventKey (MouseButton LeftButton) Down _ clickPos)
